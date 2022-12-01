052c87cacf2153aec4167cf020029a27c774a242
my git btoiandw <bbdbiw2544@gmail.com>
2022-12-01 09:17:29 +0700

    change background image path

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,10 @@ Route::get('/', function(){
 
 Auth::routes();
 
+
+Route::get('/welcome',function(){
+    return view('welcome');
+});
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
